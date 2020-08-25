c08bd03ed2c7cfb4fe6e2e222ee0628ef9a53dbe
aidil <aidil03f@gmail.com>
2020-08-25 17:46:27 +0700

    flash alert

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -23,13 +23,20 @@ class PostController extends Controller
         return view('posts.create');
     }
 
-    public function store(Request $request)
+    public function store()
     {
-       $post = new Post;
-       $post->title = $request->title;
-       $post->slug = \Str::slug($request->title);
-       $post->body = $request->body;
-       $post->save();
-       return redirect('posts');
+    //    $post = new Post;
+    //    $post->title = $request->title;
+    //    $post->slug = \Str::slug($request->title);
+    //    $post->body = $request->body;
+    //    $post->save();
+        $attr = request()->validate([
+            'title' => 'required|min:3',
+            'body'  => 'required',
+        ]);
+        $attr['slug'] = \Str::slug(request('title'));
+        Post::create($attr);
+        session()->flash('success','The post was created');
+        return redirect('posts');
     }
 }
